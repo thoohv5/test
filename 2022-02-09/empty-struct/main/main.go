176501e96d4913df9faf1f263b40cbc5eb9cfa1f
thooh <rol@thooh.com>
2022-02-09 15:15:49 +0800

    feat(test): empty struct memory alignment 2

--- 2022-02-09/empty-struct/main/main.go
+++ 2022-02-09/empty-struct/main/main.go
@@ -14,4 +14,24 @@ func main() {
 		n int8     // 1
 		m struct{} // 0
 	}{})) // 2
+
+	fmt.Println(unsafe.Sizeof(struct {
+		m struct{} // 0
+		n int64    // 8
+	}{})) // 8
+	fmt.Println(unsafe.Sizeof(struct {
+		n int64    // 8
+		m struct{} // 0
+	}{})) // 16
+
+	fmt.Println(unsafe.Sizeof(struct {
+		m struct{} // 0
+		a int8     // 1
+		n int64    // 8
+	}{})) // 16
+	fmt.Println(unsafe.Sizeof(struct {
+		n int64    // 8
+		a int64    // 1
+		m struct{} // 0
+	}{})) // 16
 }
