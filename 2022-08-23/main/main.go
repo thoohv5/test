0d58c3fec0c04e6eb7ad088ccecbdc9623ae14ad
rol <rol@thooh.com>
2022-08-26 15:33:41 +0800

    feat(test): exit

---
+++ 2022-08-23/main/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	os.MkdirAll("/Users/thooh/Sites/github.com/thoohv5/test/test", 0666)
+
+	os.Chmod("/Users/thooh/Sites/github.com/thoohv5/test/test", 0666)
+
+	file, err := os.OpenFile("/Users/thooh/Sites/github.com/thoohv5/test/test.txt", os.O_CREATE, 0666)
+	if nil != err {
+		panic(err)
+	}
+
+	err = file.Chmod(0666)
+	if nil != err {
+		panic(err)
+	}
+
+	fileInfo, err := file.Stat()
+	if nil != err {
+		panic(err)
+	}
+
+	fmt.Println(fileInfo)
+
+	continue
+
+}
